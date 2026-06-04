e275f066d71e20bc61225cd9bcd1404af51a94bc
Prashanth <prashanthprashi775@gmail.com>
2026-06-04 22:32:36 +0530

    Remove hardcoded Firebase API keys to resolve security alert

--- apps/drishti-emp/lib/firebase.ts
+++ apps/drishti-emp/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNFpkQJvBfoQbS64kkDonaFvO9V0OwbzM",
-  authDomain: "drishti-ea8bb.firebaseapp.com",
-  projectId: "drishti-ea8bb",
-  storageBucket: "drishti-ea8bb.firebasestorage.app",
-  messagingSenderId: "499412924120",
-  appId: "1:499412924120:web:d7527978c3c3cdd1b2c9ad",
-  measurementId: "G-R5NGKQ9CTM"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
